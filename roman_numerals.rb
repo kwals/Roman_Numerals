e341eebb84e601f5acd31e9e6389ae245547498f
Katie Walsh <kwals@users.noreply.github.com>
2015-01-27 20:08:22 -0500

    add suggestions from feedback
    - Got rid of extra parentheses
    - Deleted useless split line
    - Added another test to test a lot of numbers
    - Moved elsif condition to same line

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -12,38 +12,21 @@ def roman_numeral_converter numeral
       return "Invalid input."
     else
       number = 0
-    # numeral = numeral.sub(/CM/, "CCCCCCCCC")
-    # numeral = numeral.sub(/CD/, "CCCC")
-    # numeral = numeral.sub(/XL/, "XXXXXXXXX")
-    # numeral = numeral.sub(/XC/, "XXXX")
-    # numeral = numeral.sub(/IX/, "IIIIIIIII")
-    # numeral = numeral.sub(/IV/, "IIII")
-    #hash = { "CM" => "CCCCCCCCC", "CD" => "CCCC", "XL" => "XXXXXXXXX", "XC" => "XXXX", "IX" => "IIIIIIIII", "IV" => "IIII"}
-    # better_hash ={ "CM" => 900, "CD" => 400, "XL" => 90, "XC" => 40, "IX" => 9, "IV" => 4}
+      
       better_hash.each do |key,value| 
         if numeral.include?(key)
           number += value
           numeral.sub!(key, "")
         end 
       end
-    # number += roman_array.count("M") * 1000
-    # number += roman_array.count("D") * 500
-    # number += roman_array.count("C") * 100
-    # number += roman_array.count("L") * 50
-    # number += roman_array.count("X") * 10
-    # number += roman_array.count("V") * 5
-    # number += roman_array.count("I") 
-    
-    # counting_numbers = { "M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1 }
+      
       counting_numbers.each do |letter, value|
         if numeral.include?(letter)
-          numeral.split("//")
-          number += value * (numeral.count(letter)) 
+          number += value * numeral.count(letter) 
         end
       end
     end
-    elsif
-      numeral.class == Fixnum
+    elsif numeral.class == Fixnum
       number = ""
       counting_numbers.each do |key, value|
         change = numeral / value
@@ -99,4 +82,10 @@ end
     def test_invalid_input
       assert_equal roman_numeral_converter("I don't understand roman numerals."),("Invalid input.")
     end
+    
+    def test_a_whole_bunch_of_numbers
+      (1..9999).each do |n|
+        assert roman_numeral_converter(roman_numeral_converted(n)),n
+      end
+      
 end
